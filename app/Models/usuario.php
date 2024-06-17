75b7c7f8f1183a737a0281a84c3cecc753b148e0
ianbanderas <72411758+ianbanderas@users.noreply.github.com>
2024-06-17 05:12:25 +0200

    Add files via upload

--- app/Models/usuario.php
+++ app/Models/usuario.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
 
-class usuario extends Authenticatable
+
+
+class usuario extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -18,6 +21,25 @@ class usuario extends Authenticatable
     public $timestamps = false;
 
     protected $fillable = [
-        "idUsu","nombre","password","perfil"
+        "idUsu","nombre","perfil","password"
     ];
+    protected $hidden = [
+        'password',
+    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function setPasswordAttribute($value){
+        return $this->attributes['password'] = bcrypt($value);
+    }
+    
 }
